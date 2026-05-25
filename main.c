a7577b09afcc26a2a79519509b9903e629d03b1c
natharistaats <natharistaats@gmail.com>
2026-05-25 08:59:55 +0530

    Describe your changes

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main(){
     float num1,num2;
     float add,sub,mul,div;
+    char op;
 
     printf("Enter number 1: ");
     scanf("%f", &num1);
@@ -11,12 +12,30 @@ int main(){
     printf("Enter number 2: ");
     scanf("%f", &num2);
 
+    printf("Enter the operator (+ - * /): ");
+    scanf(" %c", &op);
+
+
     add = num1 + num2;
     sub = num2 - num1;
     mul = num1 * num2;
     div = num1/num2;
 
-    printf("Total value of number 1 and number 2 = %.2f", add);
+    if(op == '+'){
+        printf("Value is = %.2f", add);
+    }
+    else if(op == '-'){
+        printf("Value is = %.2f", sub);
+    }
+    else if(op == '*'){
+        printf("Value is = %.2f", mul);
+    }
+    else if(op == '/'){
+        printf("Value is = %.2f", div);
+    }
+    else{
+        printf("Invalid input. Please try again");
+    }
 
 
     return 0;
